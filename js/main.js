25d2e4a29f50b656f780e3f3422935c5ae193212
MSanbira <myt.comp@gmail.com>
2018-06-06 21:58:19 +0300

    edit butten + bug fix

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+let card = {
+    element: cardElement,
+    headline: "",
+    numOfTextBox: 0
+}
+
+let textBox = {
+    element: textBoxElement,
+    content: "",
+    members: [],
+}
+
+let member = {
+    element: memberElement,
+    fullName: "",
+    initials: "",
+}
